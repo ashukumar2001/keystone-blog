cb5e2c9ec9837b574799f9776280feaa8ec75145
Ashu Kumar <ashuk@trakiot.in>
2022-04-04 16:10:35 +0530

    d

--- keystone.ts
+++ keystone.ts
@@ -4,8 +4,8 @@ import { insertSeedData } from "./seed-data/index";
 
 export default config({
   db: {
-    provider: "sqlite",
-    url: "file:./keystone-example.db",
+    provider: "postgresql",
+    url: "postgres://kjxdtwdmagudoi:c07503cd13635529e971c5e31ce9fe5dda2cce3a26bef9fbbe501500a65007ed@ec2-44-194-4-127.compute-1.amazonaws.com:5432/d92jpgnc131jcp",
     async onConnect(context) {
       if (process.argv.includes("--seed-data")) {
         await insertSeedData(context);
