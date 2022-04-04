e5dc13add0df22c7ca5dfaf2d941b34d13d6783e
Ashu Kumar <ashuk@trakiot.in>
2022-04-04 16:51:50 +0530

    c

--- keystone.ts
+++ keystone.ts
@@ -5,7 +5,7 @@ import { insertSeedData } from "./seed-data/index";
 export default config({
   db: {
     provider: "postgresql",
-    url: "postgres://kjxdtwdmagudoi:c07503cd13635529e971c5e31ce9fe5dda2cce3a26bef9fbbe501500a65007ed@ec2-44-194-4-127.compute-1.amazonaws.com:5432/d92jpgnc131jcp",
+    url: "postgres://oxumszkqqdwlve:614f84399149784587fcea36f43583e6991e376248993b9ce64297ffa5938e89@ec2-34-192-210-139.compute-1.amazonaws.com:5432/dcq0ffirhhdh2u",
     async onConnect(context) {
       if (process.argv.includes("--seed-data")) {
         await insertSeedData(context);
